55a1c975090cb65226dd3e408934267770c600cd
AdrianGerman <adrian.german1019@gmail.com>
2024-05-30 10:02:02 -0600

    slug and markdown interpreter integration in NuxtJS

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,10 +1,47 @@
 <template>
-  <p>{{ $route.params.slug }}</p>
+  <div class="box">
+    <article class="post">
+      <h1>{{ post.title }}</h1>
+      <div class="author">
+        <p>Por {{ post.author }}</p>
+        <small>Fecha de publicación: {{ post.updated }}</small>
+      </div>
+      <p>{{ post.description }}</p>
+      <figure>
+        <img :src="post.cover" :alt="post.cover" />
+        <figcaption>Portada - {{ post.title }}</figcaption>
+      </figure>
+      <VueMarkdown class="markdown">{{ post.content }}</VueMarkdown>
+    </article>
+  </div>
 </template>
 
 <script>
+import VueMarkdown from 'vue-markdown'
 export default {
   name: 'ArticlePage',
+  components: {
+    VueMarkdown,
+  },
+  data() {
+    return {
+      post: {
+        slug: 'mi-primer-post',
+        title: 'Mi primer post',
+        author: 'Adrian German',
+        updated: '30/05/2024',
+        description: 'lorem ispum dolor sit amet',
+        cover: 'https://via.placeholder.com/1024x420',
+        content: '# Title\n\n## Second title\n\nLorem ipsum dolor sit amet',
+      },
+    }
+  },
+  head() {
+    return {
+      title: this.post?.title,
+      meta: [{ name: 'description', content: this.post?.description || '' }]
+    }
+  },
 }
 </script>
 
